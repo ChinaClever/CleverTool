36a29ed7a01b1d44154ddb7831337fb8da756180
luozhiyong <luozhiyong131@qq.com>
2019-04-04 13:25:30 +0800

    增加用户名密码

--- NetUpgradeTool/devselectwid.cpp
+++ NetUpgradeTool/devselectwid.cpp
@@ -1,5 +1,6 @@
 #include "devselectwid.h"
 #include "ui_devselectwid.h"
+#include "msgbox.h"
 
 DevSelectWid::DevSelectWid(QWidget *parent) :
     QWidget(parent),
@@ -10,6 +11,7 @@ DevSelectWid::DevSelectWid(QWidget *parent) :
     timer = new QTimer(this);
     timer->start(200);
     connect(timer, SIGNAL(timeout()),this, SLOT(timeoutDone()));
+    on_ipRadio_clicked(true);
 }
 
 DevSelectWid::~DevSelectWid()
@@ -25,10 +27,35 @@ void DevSelectWid::timeoutDone(void)
 
 void DevSelectWid::on_ipRadio_clicked(bool checked)
 {
-    if(checked) mData->devtype = 0;
+    if(checked) {
+        mData->devtype = 0;
+        ui->userEdit->setEnabled(false);
+        ui->pwdEdit->setEnabled(false);
+    }
 }
 
 void DevSelectWid::on_rzRadio_clicked(bool checked)
 {
-    if(checked) mData->devtype = 1;
+    if(checked) {
+        mData->devtype = 1;
+        ui->userEdit->setEnabled(true);
+        ui->pwdEdit->setEnabled(true);
+    }
+}
+
+void DevSelectWid::on_okBtn_clicked()
+{
+    QString str;
+    QString user =ui->userEdit->text();
+    if(user.isEmpty()) str = tr("用户名不能为空，请重新输入!!");
+
+    QString pwd = ui->pwdEdit->text();
+    if(pwd.isEmpty())  str = tr("用户名不能为空，请重新输入!!");
+
+    if(str.isEmpty()) {
+        mData->usr = user;
+        mData->pwd = pwd;
+    } else {
+        CriticalMsgBox box(this, str);
+    }
 }
